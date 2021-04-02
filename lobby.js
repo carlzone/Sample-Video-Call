ad26fedc18c223a278a768bc6798bfacd644884a
carlzone <ckevin.enoz@gmail.com>
2021-04-03 07:26:37 +0800

    1 file modified

---
+++ lobby.js
@@ -0,0 +1,20 @@
+var server = new Ably.Realtime("FdWPMQ.SWN8ug:PggMzYgwya45kdX7");
+var channel = server.channels.get("VCallRoom", { params: { rewind: "50" } });
+
+$(document).ready(function() {
+  channel.subscribe("roomList", function(msg) {
+    var room = "room-" + (Math.random() * 1000).toFixed(0).toString();
+
+    $("#roomList").append(
+      '<a href="./vroom.html#' +
+        room +
+        '" class="btn btn-obj m-3">' +
+        room.toUpperCase() +
+        "</a>"
+    );
+  });
+
+  $(document).on("click", "#createRoom", function() {
+    channel.publish("roomList", room.toUpperCase());
+  });
+});
